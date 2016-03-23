1ceca71a967ce14edb8289ed875e2e90b8552ee7
Katie Jones <katie.jones@epfl.ch>
2016-03-23 09:27:23 +0100

    Adjust output valve of compressors to get +ve SD at steady state

--- simulations/parallel/setup.m
+++ simulations/parallel/setup.m
@@ -7,10 +7,12 @@ Td = 0.0; % torque input
 u_rec = 0.0; % recycle opening
 u_d = 0.9; % discharge valve opening
 
-P_D = 1.07; % initialize tank pressure
+P_D = 1.08; % initialize tank pressure
 
 Ts = 0.05;
 
 % linearization point
   
-x_init_lin = [0.912; 1.17; 0.14; 465; 0];
+x_init_lin = [0.899; 1.126; 0.15; 440; 0];
+
+u_out = 0.55;
