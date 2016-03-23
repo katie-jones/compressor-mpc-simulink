ee2a7f8bf137cf1d4f9ac739a5988b7a38245184
Katie Jones <katie.jones@epfl.ch>
2016-03-23 15:27:05 +0100

    Fix V1 value and increase tank volume

--- simulations/parallel/const_flow.m
+++ simulations/parallel/const_flow.m
@@ -6,7 +6,7 @@ a = 340; % speed of sound
 Pin = 1; % pressure at inlet
 Ptank = -1; % pressure in tank -- shouldn't use
 
-V1 = pi * (0.60 / 2)^2 * 2 + pi * (0.08 / 2)^2 * 8.191; % volume of the inlet pipe, 6m
+V1 = 2*pi * (0.60 / 2)^2 * 2 + pi * (0.08 / 2)^2 * 8.191; % volume of the inlet pipe, 6m
 V2 =  pi * (0.60 / 2)^2 * 2 + pi * (0.08 / 2)^2 * 5.940; % volume, tank
 
 AdivL =     pi * (0.08 / 2)^2 / 3 * 0.1;  % Duct cross section divided by length
