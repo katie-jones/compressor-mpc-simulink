e3db6a997cddf9c4fa6f69b39923e80bfa4abb41
Katie Jones <katie.jones@epfl.ch>
2016-04-06 11:42:36 +0200

    Add input disturbance results

--- simulations/common/make_plots_parallel.m
+++ simulations/common/make_plots_parallel.m
@@ -2,11 +2,10 @@
 set(0,'DefaultAxesFontSize',16)
 set(0,'DefaultLineLineWidth',2)
 
-save_plots = 0;
-
 fig=figure;
 % set(fig,'units','centimeters','position',[10 10 10 7])
 plot(pout.time,pout.signals.values+1.126);
+grid on
 title('Compressor outlet pressure')
 xlabel('Time [s]')
 ylabel('Pressure [bar?]')
@@ -15,13 +14,14 @@ set(leg,'fontsize',14,'location','best')
 
 if save_plots
     fig=printplot(fig);
-    saveas(fig,'results/output_dist/pout.pdf')
+    saveas(fig,[resultsdir,'/pout.pdf'])
 end
 
 
 fig=figure;
 plot(PD.time,PD.signals.values); hold on
 plot([min(PD.time),max(PD.time)],[yref(4),yref(4)],':k')
+grid on
 title('Tank pressure')
 xlabel('Time [s]')
 ylabel('Pressure [bar?]')
@@ -29,13 +29,14 @@ leg = legend('Value','Reference');
 set(leg,'fontsize',14,'location','best')
 if save_plots
     fig=printplot(fig);
-    saveas(fig,'results/output_dist/pd.pdf')
+    saveas(fig,[resultsdir,'/pd.pdf'])
 end
 
 
 fig=figure; 
 plot(SD.time,SD.signals.values);
 hold on
+grid on
 plot([min(SD.time),max(SD.time)],[yref(1),yref(1)],':k')
 title('Surge distance')
 xlabel('Time [s]')
@@ -44,6 +45,6 @@ leg=legend('Compressor 1','Compressor 2','Reference');
 set(leg,'fontsize',14,'location','southeast')
 if save_plots
     fig=printplot(fig);
-    saveas(fig,'results/output_dist/sd.pdf')
+    saveas(fig,[resultsdir,'/sd.pdf'])
 end
 
