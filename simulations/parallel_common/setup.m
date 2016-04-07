895b146697a5f5588139814131cb6d9ca67be35c
Katie Jones <katie.jones@epfl.ch>
2016-04-07 09:46:23 +0200

    Move parallel plant to library block

--- simulations/parallel_common/setup.m
+++ simulations/parallel_common/setup.m
@@ -1,21 +1,18 @@
 % Setup for parallel compressor simulation
 clear all
-global P_D 
 addpath('../common')
 
-% Td = [zeros(100,1); 0.1]; % torque input
 Td = 0;
 u_rec = 0.0; % recycle opening
-u_d = 0.7; % discharge valve opening
 
 P_D = 1.08; % initialize tank pressure
-
-Ts = 0.05;
 
 % linearization point
   
 x_init_lin = [0.899; 1.126; 0.15; 440; 0];
 
-uoff1 = [0.304 0.43 1 0 0];
-uoff2 = uoff1;
+[Ts, xsize_comp, xsize, usize_comp, ysize, uoff1, uoff2, ud] = const_sim();
 
+u_d = ud;
+
+n_delay = [0; 40];
