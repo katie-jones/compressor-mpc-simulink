5c363e9cdd80331236ad4ec7040006f71e619266
Katie Jones <katie.jones@epfl.ch>
2016-05-02 17:03:16 +0200

    Fix test to work with revised functions

--- simulations/parallel_common/test_linearization.m
+++ simulations/parallel_common/test_linearization.m
@@ -22,9 +22,9 @@ u = [uoff; uoff; 0] + [Td(1); 0; 0; u_rec(1); 0; Td(2); 0; 0; u_rec(2); 0; u_d];
 x = [x_init_lin; x_init_lin; P_D];
 
 % initial derivatives
-f1 = get_comp_deriv(x_init_lin,[u(1:usize);P_D],1);
-f2 = get_comp_deriv(x_init_lin,[u(usize+1:2*usize);P_D],1);
-ftank = get_tank_deriv(x,u);
+[f1,m1] = get_comp_deriv(x_init_lin,[u(1:usize-1);P_D],1);
+[f2,m2] = get_comp_deriv(x_init_lin,[u(usize+1:2*usize-1);P_D],1);
+ftank = get_tank_deriv(P_D,[m1+m2;u_d]);
 
 % linearize system
 [Ac,Bc,Ccorig] = linearize_tank(x, u);
@@ -42,13 +42,13 @@ fest = f;
 
 for i=1:N
     P_D = P_D_init + delta_x(end,i);
-    fe1 = get_comp_deriv(x_init_lin+delta_x(1:xsize,i),[u(1:usize)+delta_u(1:usize,i);P_D],1);
-    fe2 = get_comp_deriv(x_init_lin+delta_x(xsize+1:2*xsize,i),[u(usize+1:2*usize)+delta_u(usize+1:2*usize,i);P_D],1);
-    fetank = get_tank_deriv(x+delta_x(:,i),u+delta_u(:,i));
+    [fe1,m1] = get_comp_deriv(x_init_lin+delta_x(1:xsize,i),[u(1:usize-1)+delta_u(1:usize-1,i);P_D],1);
+    [fe2,m2] = get_comp_deriv(x_init_lin+delta_x(xsize+1:2*xsize,i),[u(usize+1:2*usize-1)+delta_u(usize+1:2*usize-1,i);P_D],1);
+    fetank = get_tank_deriv(x(end)+delta_x(end,i),[m1+m2,u(end)+delta_u(end,i)]);
     
-    f(i,:) = [fe1-f1; fe2-f2; fetank-ftank];
+    f(i,:) = [fe1; fe2; fetank];
     
-    fest(i,:) = Ac*delta_x(:,i) + Bc*delta_u([1,4,usize+1,usize+4],i);
+    fest(i,:) = [f1;f2;ftank] + Ac*delta_x(:,i) + Bc*delta_u([1,4,usize+1,usize+4],i);
     
 end
 close all
