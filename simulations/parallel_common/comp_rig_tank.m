e33d7b04ae3e16d785fedd6e8b381e5eece3aee6
Katie Jones <katie.jones@epfl.ch>
2016-05-02 14:48:28 +0200

    Use get_tank_deriv function

--- simulations/parallel_common/comp_rig_tank.m
+++ simulations/parallel_common/comp_rig_tank.m
@@ -87,28 +87,6 @@ sys(2) = P_D; % p2
 
 
 function sys = mdlDerivatives(t,x,u,Param)
-global P_D
+sys = get_tank_deriv(x,u);
 
-% States
-P_D = x(1);
-
-% Inputs
-m_in = u(1);
-Outflow_opening = u(2);
-
-[Out_pres,VolumeT,D2,m_out_c] = const_tank();
-
-SpeedSound = const_flow();
-
-
-dp_sqrt2 = sqrt(abs(P_D*100 - Out_pres*100)) * sign(P_D*100 - Out_pres*100);      
-% M4 = [dp_sqrt2.^2.*u_out.^3 dp_sqrt2.^2.*u_out.^2 dp_sqrt2.^2.*u_out dp_sqrt2.^2 ...
-%       dp_sqrt2.*u_out.^3 dp_sqrt2.*u_out.^2 dp_sqrt2.*u_out dp_sqrt2 ...
-%       u_out.^3 u_out.^2 u_out ones(length(u_out),1)]';
-M5 = [dp_sqrt2*Outflow_opening^3 dp_sqrt2*Outflow_opening^2 dp_sqrt2*Outflow_opening dp_sqrt2 ...
-       Outflow_opening^3 Outflow_opening^2 Outflow_opening 1]';
-% m_out = D * M4 + 0.05;
-m_out = D2 * M5 + m_out_c;
-
-sys(1) = SpeedSound * SpeedSound / VolumeT * (m_in - m_out) * 1e-5; % p1
 
