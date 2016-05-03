b094fff60b9920f2eadc012b89577b2b1f2924ec
Katie Jones <katie.jones@epfl.ch>
2016-05-03 13:08:47 +0200

    Revert "Remove tank from OL simulation"
    This reverts commit 28935b5d3675f1df3a8192a9c2ef4b468957df4f.

--- simulations/parallel_common/setup.m
+++ simulations/parallel_common/setup.m
@@ -2,18 +2,18 @@
 clear all
 addpath('../common')
 
-Td1 = [0 0];
+Td1 = [0 0;
+    5 0.1];
 Td2 = [0 0];
 ur1 = [0 0];
 ur2 = [0 0];
 
 % linearization point
   
-xinit = [0.887; 1.086; 0.161; 421; 0; 
-    0.938; 1.148; 0.161; 421; 0];
+xinit = [0.890; 1.096; 0.159; 426; 0; 
+    0.928; 1.142; 0.159; 426; 0];
 
 [Ts, xsize_comp, xsize, usize_comp, ysize, uoff1, uoff2] = const_sim();
-
 
 ud = 0.7;
 
