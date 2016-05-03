d9fc37a5403836bd9b1ea7eb7e7599444564a575
Katie Jones <katie.jones@epfl.ch>
2016-05-03 09:12:53 +0200

    Tune centralized controller

--- simulations/centralized/weights.m
+++ simulations/centralized/weights.m
@@ -1,7 +1,7 @@
 [~,~,~,p,m] = const_mpc();
 
-UW = [4e3 6e4];
-YW = [1 1 0.1 5e3];
+UW = [2e4 2e5];
+YW = [1 1 0.1 5e2];
 
 UWT = kron(eye(m),diag([UW,UW]'));
 % UWT = kron([1, 0; 0, 60],diag([UW,UW]));
