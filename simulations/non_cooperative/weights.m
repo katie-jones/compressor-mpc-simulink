7ff7d79e229a9fcde237d063e543c01ca98c14b7
Katie Jones <katie.jones@epfl.ch>
2016-06-21 11:22:25 +0200

    Use separate y weights

--- simulations/non_cooperative/weights.m
+++ simulations/non_cooperative/weights.m
@@ -1,7 +1,9 @@
 [~,~,~,p,m] = const_mpc();
 
-UW = 1e4*[1e2 1e3];
-YW = [1000 1];
+UW = 1e5*[1e2 1e3];
+YW1 = [1000 10];
+YW2 = [1000 10];
 
 UWT = kron(eye(m),diag([UW]'));
-YWT = kron(eye(p),diag(YW'));
+YWT1 = kron(eye(p),diag(YW1'));
+YWT2 = kron(eye(p),diag(YW2'));
