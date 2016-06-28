4b08bd9956c591a2c1b5aa95db87d8c80dcd9134
Katie Jones <katie.jones@epfl.ch>
2016-06-28 12:54:59 +0200

    tmp

--- simulations/non_cooperative/weights.m
+++ simulations/non_cooperative/weights.m
@@ -1,8 +1,8 @@
 [~,~,~,p,m] = const_mpc();
 
-UW = 1e5*[1e2 1e3];
-YW1 = [1000 10];
-YW2 = [1000 10];
+UW = 1e4*[1e2 1e3];
+YW1 = [1e3 10];
+YW2 = [1e4 1e2];
 
 UWT = kron(eye(m),diag([UW]'));
 YWT1 = kron(eye(p),diag(YW1'));
